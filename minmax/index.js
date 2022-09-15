505259abf7388bf31213d9c09f1e71d1a1cd9afc
Amit <amitkr24031995@gmail.com>
2022-09-15 10:47:55 +0530

    modified is min max problem

--- minmax/index.js
+++ minmax/index.js
@@ -4,24 +4,35 @@
 
 let Input= [5, 2, 7, 9, 13, 3, 8];
     let max=Input[0];
+    let index1;
 function maxnumber(){
     for(let i=0;i<Input.length;i++){
         if(Input[i]>max){
             max=Input[i]
         }
+        if(Input[i]==max){
+            index1=i
+        }
     }
     
 }
 maxnumber();
+console.log(index1);
 
 let min=Input[0];
+let index2;
 function minnumber(){
     for(let i=0;i<Input.length;i++){
         if(Input[i]<min){
             min=Input[i]
         }
+        if(Input[i]==min){
+            index2=i
+        }
     }
     
 }
 minnumber();
-console.log(Input.indexOf(max)-Input.indexOf(min));
+console.log(index2);
+// console.log(Input.indexOf(max)-Input.indexOf(min));
+console.log(index1-index2);
